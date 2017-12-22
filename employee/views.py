e797e4808a4c4f6edc4bd3c75650e9d0278b25f4
Diwaker Ghimire <idwaker@gmail.com>
2017-12-22 09:48:46 +0545

    added simple view and template

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from employee.models import Employee
 
 # Create your views here.
+
+def homepage(request):
+    employees = Employee.objects.all()
+    return render(template_name='base.html', request=request, context={'employees':employees})
--- firstcrm/urls.py
+++ firstcrm/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from employee.views import homepage
+
 urlpatterns = [
+    path('', homepage, name='homepage'),
     path('admin/', admin.site.urls),
 ]
